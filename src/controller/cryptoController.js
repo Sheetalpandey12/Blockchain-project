e6934d85a1aee4d53b6be00482226a24dc7ddf3c
yashrajsinh09 <yashrajsinh2727@gmail.com>
2022-12-14 16:37:06 +0530

    final

--- src/controller/cryptoController.js
+++ src/controller/cryptoController.js
@@ -1,24 +1,86 @@
 const axios = require("axios");
 const url = "http://api.coincap.io/v2/assets";
+const cryptoModel = require('../models/cryptoModel')
+
+// const getblockChain = async function (req, res) {
+//   try {
+//       const webapi = {
+//           method: "GET",
+//           url: 'https://api.coincap.io/v2/assets/',
+//           headers: { Authorization: "Bearer f4822a6e-7c25-4bc4-93fc-bf6cbb22a6f1" }
+//       }
+
+//       let result = await axios(webapi)
+      
+
+//       data = result.data.data
+//       //sort according to growth
+//       const sortdata = data.sort((a, b) => { return a.changePercent24Hr - b.changePercent24Hr });
+//       console.log(sortdata)
+      
+//       // delete old data
+//       await cryptoModel.deleteMany()
+      
+//       // recreate create data
+//       const finaldata = await cryptoModel.create(sortdata)
+      
+//       final = await cryptoModel.find().select({__v:0, _id:0})
+
+//       res.status(200).send({ status: true, data: final })
+
+//   } catch (error) {
+//       res.status(500).send({ status: false, message: error.message })
+//   }
+// }
 
 
-const getAssets = async (req,res) => {
-  try{
-  const response = await axios.get(url);
-  return response.data.data
-    .sort((c1, c2) => {
-      const { changePercent24Hr: p } = c1;
-      const { changePercent24Hr: p2 } = c2;
-      return p - p2;
-    })
-    .map((c) => {
-      const { priceUsd, symbol, name, marketCapUsd } = c;
-      return { priceUsd, symbol, name, marketCapUsd };
-    });
-  }
-  catch(err){
-    return res.status(500).send({message: err.message})
-  }
-};
- 
-module.exports = {getAssets}
+
+const getblockChain = async function (req,res) {
+    try{
+         const webapi ={
+            method : "GET",
+            url : "https://api.coincap.io/v2/assets/",
+            headers : { Authorization : "Bearer f4822a6e-7c25-4bc4-93fc-bf6cbb22a6f1"}
+         }
+
+         let result = await axios (webapi)
+
+         data = result.data.data
+
+         const sortdata = data.sort((a,b) => {return a.changePercent24Hr - b.changePercent24Hr});
+
+         await cryptoModel.deleteMany()  //delete all data
+
+         const finaldata = await cryptoModel.create(sortdata)  // create data
+
+         const final = await cryptoModel.find().select({__v:0,_id:0})  
+         return res.status(200).send({status:true, data: sortdata})
+    }
+    catch(err){
+         return res.status(500).send({status:false, message: err.message})
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {getblockChain}
--- src/router/route.js
+++ src/router/route.js
@@ -1,29 +1,7 @@
 const router = require("express").Router();
-const { getAssets } = require("../controller/cryptoController");
+const { getblockChain } = require("../controller/cryptoController");
 const cryptoModel = require("../models/cryptoModel");
 
-router.get("/crypto", async (req,res) => {
-  let data = await getAssets();
-
-  const promises = data.map((c) => {
-    return cryptoModel.findOneAndUpdate(
-      { name: c.name },
-      { $set: c},
-      {
-        upsert: true,
-        new: true,
-      } 
-    );
-  });
-  Promise.all(promises).then((data) => {
-    cryptoModel.deleteMany({
-      _id: {
-        $nin: data.map((i) => i._id),
-      },
-    }).then(console.log);
-    res.json(data);
-  });
-
-});
+router.get("/crypto",getblockChain )
 
 module.exports = {router}
